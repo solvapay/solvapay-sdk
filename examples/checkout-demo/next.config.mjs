44fa6e6e93c8072a0ea5566c670fc3e3289b931b
jacksmithinsulander <666.jack.smith@protonmail.com>
2026-05-27 22:59:22 +0200

    fix(examples): lint

--- examples/checkout-demo/next.config.mjs
+++ examples/checkout-demo/next.config.mjs
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
+/* global process */
 const nextConfig = {
   env: {
     NEXT_PUBLIC_SOLVAPAY_PRODUCT_REF: process.env.SOLVAPAY_PRODUCT_REF,
--- examples/tailwind-checkout/next.config.mjs
+++ examples/tailwind-checkout/next.config.mjs
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
+/* global process */
 const nextConfig = {
   transpilePackages: [
     '@solvapay/auth',
